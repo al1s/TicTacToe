19b2f3fcf3093008535c1e17bcec1f16ff8afcf8
Alstof <alstof@gmail.com>
2018-02-19 08:01:28 -0800

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,7 @@
+/* eslint no-var: 0 */
+/* eslint vars-on-top: 0 */
+/* eslint prefer-const: 0 */
+/* eslint arrow-parens: 0 */
+
+// checkAvailableLine - do I have the line with only my signs and empty fields?
+// checkTreats - do the opponent have two signs in line?
